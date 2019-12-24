e46e65438a3e6cc22a5c344ba8cc90b76ccacfb2
cmbeyer <bigbeyer03221995@gmail.com>
2019-12-24 15:21:04 -0500

    2

--- folderchk.php
+++ folderchk.php
@@ -1,8 +1,15 @@
 <?php
-$dir    = '/Media/Movies';
-$files1 = scandir($dir);
-$files2 = scandir($dir, 1);
 
-print_r($files1);
-print_r($files2);
+// set up basic connection
+$conn_id = ftp_connect('69.114.34.40');
+
+// login with username and password
+$login_result = ftp_login($conn_id, 'chris', '14752369Cb');
+
+// get contents of the current directory
+$contents = ftp_mlsd($conn_id, ".");
+
+// output $contents
+var_dump($contents);
+
 ?>
