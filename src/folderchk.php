cc95dbf90aa013df63895c23e77ef67988128cb0
cmbeyer <bigbeyer03221995@gmail.com>
2020-01-02 21:50:40 -0500

    Docker with Github setup

--- src/folderchk.php
+++ src/folderchk.php
@@ -1,17 +1,8 @@
 <?php
+$dir    = '/';
+$files1 = scandir($dir);
+$files2 = scandir($dir, 1);
 
-// set up basic connection
-$conn_id = ftp_connect('69.114.34.40');
-
-// login with username and password
-$login_result = ftp_login($conn_id, 'chris', '14752369Cb');
-
-// get contents of the current directory
-$contents = ftp_mlsd($conn_id, "/var/www/html");
-
-// output $contents
-var_dump($contents);
-
-echo ' Here ';
+print_r($files1);
 
 ?>
