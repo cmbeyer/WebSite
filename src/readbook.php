8c191fa3d64da2762ee7cc4ab77c977758263975
cmbeyer <bigbeyer03221995@gmail.com>
2020-01-08 21:28:45 -0500

    Docker with Github setup

--- src/readbook.php
+++ src/readbook.php
@@ -26,14 +26,12 @@ echo $_POST["book"];
 <div id="book">
     <?php
     $directory = "/var/www/html/Media/Web Programming books/Books for Certs/".$_POST["book"];
-
+echo $directory;
     // Open a directory, and read its contents
     if (is_dir($directory)){
         if ($opendirectory = opendir($directory)){
             while (($file = readdir($opendirectory)) !== false) {
-                if (strpos($file, '.') === false) {
                     echo '<div style="background-image:url('.$file.');"></div>>';
-                }
             }
             closedir($opendirectory);
         }
