e13b190c9c20708b4111667c559dcacc3a575aee
cmbeyer <bigbeyer03221995@gmail.com>
2020-01-08 20:50:43 -0500

    Docker with Github setup

--- src/folderchk.php
+++ src/folderchk.php
@@ -1,4 +1,4 @@
-<form action="readbook.php">
+<form action="readbook.php" method="POST">
 
 <?php
 $directory = "/var/www/html/Media/Web Programming books/Books for Certs";
@@ -8,7 +8,7 @@ if (is_dir($directory)){
     if ($opendirectory = opendir($directory)){
         while (($file = readdir($opendirectory)) !== false) {
             if (strpos($file, '.') === false) {
-                echo '<button type="submit" class="button" value="'.$file.'">';
+                echo '<button type="submit" class="button" name=book" value="'.$file.'">';
                 echo "filename:" . $file . "<br>";
             }
         }
@@ -17,4 +17,4 @@ if (is_dir($directory)){
 }
 ?>
 </form>
-<a href
+
