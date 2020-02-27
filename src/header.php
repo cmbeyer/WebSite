9adc7f9f599a8b9d6603276532d480922685b77c
cmbeyer <bigbeyer03221995@gmail.com>
2020-02-26 20:14:45 -0500

    Turn.js image set up. With Header and footer

--- src/header.php
+++ src/header.php
@@ -85,9 +85,7 @@ function sendNotification(){
     
     
 </head>
-<?php
-session_start();
-?>
+
 <body>
 <script src="https://static.codepen.io/assets/common/stopExecutionOnTimeout-de7e2ef6bfefd24b79a3f68b414b87b8db5b08439cac3f1012092b2290c719cd.js"></script>
 <script src="https://codepen.io/darrylhuffman/pen/eevREZ.js"></script>
--- src/readbook.php
+++ src/readbook.php
@@ -1,12 +1,4 @@
 <?php include 'header.php'; ?>
-
-
-<!doctype html>
-<!--[if lt IE 7 ]> <html lang="en" class="ie6"> <![endif]-->
-<!--[if IE 7 ]>    <html lang="en" class="ie7"> <![endif]-->
-<!--[if IE 8 ]>    <html lang="en" class="ie8"> <![endif]-->
-<!--[if IE 9 ]>    <html lang="en" class="ie9"> <![endif]-->
-<!--[if !IE]><!--> <html lang="en"> <!--<![endif]-->
 <head>
 <meta name="viewport" content="width = 1050, user-scalable = no" />
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
@@ -17,7 +9,7 @@
 <!--<div class="flipbook-viewport">
 	<div class="container">
 		<div class="flipbook">
-		-->
+
    <?php
     $directory = "/var/www/html/Media/Web Programming books/Books for Certs/".$_POST["book"];
     $directory2="http://69.114.34.40/Media/Web Programming books/Books for Certs/".$_POST["book"];
@@ -36,23 +28,35 @@ echo $directory;
         }
     }
     ?>
-
-
-	<!--	</div>
+	</div>
 	</div>
 </div>--->
 
 <div id="flipbook">
     <div class="hard"> Turn.js </div>
     <div class="hard"></div>
-    <div>Page 1 </div>
-    <div> Page 2 </div>
-    <div> Page 3 </div>
-    <div> Page 4 </div>
+    <?php
+    $directory = "/var/www/html/Media/Web Programming books/Books for Certs/".$_POST["book"];
+    $directory2="http://69.114.34.40/Media/Web Programming books/Books for Certs/".$_POST["book"];
+    echo $directory;
+    // Open a directory, and read its contents
+    if (is_dir($directory)){
+        if ($opendirectory = opendir($directory)){
+            while (($file = readdir($opendirectory)) !== false) {
+                if (strpos($file, '.jpeg') ==! false) {
+                    //include('pagestoread.php');
+                    echo "<div>".$directory2."/".$file;
+                }
+
+            }
+            closedir($opendirectory);
+        }
+    }
+    ?>
     <div class="hard"></div>
     <div class="hard"></div>
 </div>
-<img src="<?php echo $directory2."/page_1.jpeg";?>"> </img>
+<!--<img src="<?php echo $directory2."/page_1.jpeg";?>"> </img>-->
 
 
 <script type="text/javascript">
