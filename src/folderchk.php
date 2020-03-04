d9645319303768f9c2e0513be1a655fcd34ae753
cmbeyer <bigbeyer03221995@gmail.com>
2020-03-04 18:34:31 -0500

    Turn.js image set up. With Header and footer

--- src/folderchk.php
+++ src/folderchk.php
@@ -27,7 +27,7 @@ if (is_dir($directory)){
     if ($opendirectory = opendir($directory)){
         while (($file = readdir($opendirectory)) !== false) {
             if (strpos($file, '.') === false) {
-                echo '<input type="submit" name="book" value="'.$file.'">';
+                echo '<input type="submit" name="manga" value="'.$file.'">';
                 echo "filename:" . $file . "<br>";
             }
         }
