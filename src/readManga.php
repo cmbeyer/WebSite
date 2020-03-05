210ed3d4ad7a40f9cd18cf04e55cb5b1cc53457f
cmbeyer <bigbeyer03221995@gmail.com>
2020-03-04 19:42:12 -0500

    Turn.js image set up. With Header and footer

--- src/readManga.php
+++ src/readManga.php
@@ -40,17 +40,6 @@
                 }
             }
             ?>
-
-
-            <button id="readNow" comic_title="zack_and_max.cbz">
-                <img src='./comics/zack_and_max_thumb.jpg' class='imgUrl'/>
-                <br>The Adventures of Zack &amp; Maxx
-            </button>
-
-            <button id="readNow" comic_title="comic_strip.cbz">
-                <img src='./comics/comic_strip_thumb.jpg' class='imgUrl'/>
-                <br>Alien Comic Strip
-            </button>
         </div>
         <br>
     </fieldset>
