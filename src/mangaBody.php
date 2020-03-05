4e401caf9531fd27140b4ca21fcf35c5fe8268d8
cmbeyer <bigbeyer03221995@gmail.com>
2020-03-04 19:22:52 -0500

    Turn.js image set up. With Header and footer

--- src/mangaBody.php
+++ src/mangaBody.php
@@ -1,6 +1,6 @@
 <?php
 ?>
 <button id="readNow"  manga="<?php echo $_POST['manga']?>" volume="<?php echo $VolumeFinal?>" comic_title="<?php echo $mangaName?>">
-    <img src='http://69.114.34.40/Media/Manga/<?php echo $_POST['manga']."/".$volumeFinal."/cover.jpg";?>' class='imgUrl'/>
+    <img src='http://69.114.34.40/Media/Manga/<?php echo $_POST['manga']."/".$VolumeFinal."/cover.jpg";?>' class='imgUrl'/>
     <br>Dragon Ball Super
 </button>
